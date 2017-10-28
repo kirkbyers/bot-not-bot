83a938b60e642a56cc3948d07eb04c2531170a1d
kirkbyers <kirklbyers@gmail.com>
2017-10-28 15:05:14 -0500

    Auth gaurd metadata route

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const metadata = require('./metadata');
 const users = require('./users');
 
+const { authenticateRoutes } = require('../middleware');
+
 const router = express.Router();
 
-router.use('/api', metadata);
+router.use('/api', authenticateRoutes, metadata);
 router.use('/api', users);
 
 module.exports = router;
