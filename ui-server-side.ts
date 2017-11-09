f911fb27c46d5769c6ccefe59769d777058ae02b
kirkbyers <kirklbyers@gmail.com>
2017-11-08 23:05:58 -0600

    Fix next serving from dist

--- ui-server-side.ts
+++ ui-server-side.ts
@@ -2,6 +2,6 @@ import * as Next from 'next';
 
 // TODO: figure out prod mode when not using next default server
 // const dev = process.env.NODE_ENV !== 'production';
-const uiApp = Next({ dev: true });
+const uiApp = Next({ dir: './dist', dev: true });
 
 export { uiApp };
