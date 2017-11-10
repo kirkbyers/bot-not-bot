790cee1d09beb5f4340936797704f2794f62152f
kirkbyers <kirklbyers@gmail.com>
2017-11-09 19:58:42 -0600

    Fix production config

--- ui-server-side.ts
+++ ui-server-side.ts
@@ -1,7 +1,7 @@
 import * as Next from 'next';
 
 // TODO: figure out prod mode when not using next default server
-// const dev = process.env.NODE_ENV !== 'production';
-const uiApp = Next({ dir: './dist', dev: true });
+const dev = process.env.NODE_ENV !== 'production';
+const uiApp = Next({ dir: './dist', dev });
 
 export { uiApp };
